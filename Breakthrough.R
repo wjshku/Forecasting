4816c6cc2faee924a4c448d057c87e681ce7ed09
wjshku <55918694+wjshku@users.noreply.github.com>
2022-02-16 16:49:56 +0800

    Add files via upload

---
+++ Breakthrough.R
@@ -0,0 +1,54 @@
+#A function that outputs the probability of Poi large than n
+poi <- function(lambda, n){
+  p <- 1
+  if(n <= 0){
+    return(p)
+  }
+  for(i in 1:n){
+    p <- p - exp(-lambda)*lambda^(i)/factorial(i)
+  }
+  return(p)
+}
+
+alpha <- 10
+beta <- 2
+lambda_hat <- (4+alpha)/(beta+16)
+
+#starting time is 1991
+Nbytime <- c(1,2,3,4,5,6,7,8,9)
+
+#Total Number of breakthroughs recognized at the end of year tp
+NumofBreak <- function(breakthroughs,tp){
+  for(i in length(breakthroughs):1){
+    if(breakthroughs[i] <= tp){
+      return(i)
+    }
+  }
+}
+
+prob_before <- function(tp, lambda_hat, np, n_break){
+  prob <- poi((2021-tp)*lambda_hat,n_break-np-1)
+  return(prob)
+}
+
+lambda_esti <- function(alpha, beta, tp, np){
+  period <- tp - 1991 + 1
+  return((np+alpha)/(beta+period))
+}
+
+#Run the program here
+Breakthroughs1 <- c(1991,2001,2003,2013,2017,2018,2019)
+Breakthroughs <- c(1991,2001,2003,2008,2013,2017,2018,2019,2019,2020,2020)
+
+n_breaks <- 7:15
+t <- 2016:2020
+trials <- data.frame(matrix(rep(1,length(n_breaks)*length(t)),ncol = length(t)), row.names = n_breaks)
+colnames(trials) <- t
+for(i in 1:length(n_breaks)){
+  for(j in 1:length(t)){
+    np <- NumofBreak(Breakthroughs,t[j])
+    lambda_hat <- lambda_esti(alpha, beta, t[j], np)
+    trials[i,j] <- prob_before(t[j],lambda_hat, np, n_breaks[i])
+  }
+}
+round(trials,2)
